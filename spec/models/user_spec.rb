33bdd3ebf9130f654b8dc6e2509fc157be8b882f
fmbf <circumnavigate.the.globe@gmail.com>
2018-05-05 11:06:24 -0700

    write User model & specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -24,6 +24,6 @@ RSpec.describe User, type: :model do
   it { should validate_length_of(:password).is_at_least(6) }
   it { should validate_presence_of(:username) }
   it { should validate_presence_of(:password_digest) }
-  it { should have_many(:links) }
+  it { should have_many(:beers) }
   it { should have_many(:comments) }
 end
